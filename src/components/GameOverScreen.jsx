2604221b036bafb910e18903c90a103fac4c8d8c
STAMBOLIYSKI, ATANAS (EXTERN) <atanas.stamboliyski.sp+lhgroup@lht.dlh.de>
2026-04-26 20:03:45 +0300

    Polish game over screen and bump version

--- src/components/GameOverScreen.jsx
+++ src/components/GameOverScreen.jsx
@@ -3,11 +3,11 @@ import gameOverImage from '../assets/images/game-over.png';
 
 export default function GameOverScreen({ winnings, onRestart, onHome }) {
   return (
-    <div className="screen end-screen">
-      <h1 className="end-title">Game over</h1>
-      <img className="end-image" src={gameOverImage} alt="Game over" />
-      <p className="end-warning">You will be next for TDP enjoy :)</p>
-      <div className="end-actions">
+    <div
+      className="screen end-screen game-over-screen"
+      style={{ backgroundImage: `url(${gameOverImage})` }}
+    >
+      <div className="end-actions game-over-actions">
         <button className="btn btn-primary" onClick={onRestart}>Play again</button>
         <button className="btn btn-ghost" onClick={onHome}>Main menu</button>
       </div>
